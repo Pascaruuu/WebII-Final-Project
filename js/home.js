caeb53ce23f5226fe69715c09cf6411dc9357f48
Pascal Tuy <2022025tuy@aupp.edu.kh>
2023-04-30 00:33:19 +0700

    update

--- js/home.js
+++ js/home.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
     
     $.ajax({
       type: 'POST',
-      url: 'http://localhost:8000/process.php', // replace with the URL of your processing script
+      url: 'http://localhost/php/contact.php', 
       data: form_data
     })
     .done(function(response) {
